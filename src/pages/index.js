7cca877380f3861f88545c3f602770264f4a8959
Sourabh Subhod <sourabhsubhod@gmail.com>
2020-03-28 19:35:26 +0000

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { navigate } from 'gatsby';
+import { Router, Redirect } from "@reach/router"
+import {isLoggedIn} from "utils/auth"
 
 export default () => {
-  useEffect(() => {
-    navigate('/app/');
-  }, []);
-  return null;
+  // useEffect(() => {
+  //   navigate('/app/');
+  // }, []);
+  return (
+    <Router>
+      <Redirect noThrow from="/" to={isLoggedIn() ? "app/dashboard" : "app/sign-in"}/>
+    </Router>
+  )
 };
